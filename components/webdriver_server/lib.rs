74dc09582aabd8dc5b285085190904dec8540207
Manish Goregaokar <manishsmail@gmail.com>
2015-03-19 19:45:51 +0530

    Cherry-pick the rustup_mini branch

--- components/webdriver_server/lib.rs
+++ components/webdriver_server/lib.rs
@@ -35,7 +35,6 @@ use util::task::spawn_named;
 use uuid::Uuid;
 
 use std::borrow::ToOwned;
-use std::net::IpAddr;
 use rustc_serialize::json::{Json, ToJson};
 use std::collections::BTreeMap;
 
@@ -43,7 +42,7 @@ pub fn start_server(port: u16, constellation_chan: ConstellationChan) {
     let handler = Handler::new(constellation_chan);
 
     spawn_named("WebdriverHttpServer".to_owned(), move || {
-        server::start(IpAddr::new_v4(0, 0, 0, 0), port, handler);
+        server::start("0.0.0.0", port, handler);
     });
 }
 
