cf7d9a44d8089403dad357a18072fdda558374ce
Lars Bergstrom <lars@lars.com>
2015-04-24 17:14:34 -0500

    Canvas builds

--- components/canvas/lib.rs
+++ components/canvas/lib.rs
@@ -3,8 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #![feature(collections)]
+#![feature(convert)]
 #![feature(core)]
-#![feature(std_misc)]
 
 extern crate azure;
 extern crate cssparser;
